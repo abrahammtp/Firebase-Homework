5dc6e0a986c5f23866b735ba7b0f1578dc7a40a4
Semi Western <46465000+abrahammtp@users.noreply.github.com>
2019-03-13 23:12:05 -0400

    Finished comments on the files, and changed some lines of code on app.js

--- assets/JavaScript/app.js
+++ assets/JavaScript/app.js
@@ -1,84 +1,3 @@
-// var config = {
-//     apiKey: "AIzaSyCAlUs1o7-3cpFCXQ_f_J7ymRlfe6N6myQ",
-//     authDomain: "train-tracker-03-12-19.firebaseapp.com",
-//     databaseURL: "https://train-tracker-03-12-19.firebaseio.com",
-//     projectId: "train-tracker-03-12-19",
-//     storageBucket: "train-tracker-03-12-19.appspot.com",
-//     messagingSenderId: "413897219999"
-// };
-// firebase.initializeApp(config);
-
-// var database = firebase.database();
-
-// // Initial Values
-// var trainName = "";
-// var destination = "";
-// var firstTrainTime = 0;
-// var frequency = 0;
-// var nextArrival = 0;
-// var minutesAway = 0;
-
-// // Capture Button Click
-// $("#add-train-btn").on("click", function (event) {
-//     event.preventDefault();
-
-//     // Grabbed values from text boxes
-//     trainName = $("#train-name-input").val().trim();
-//     destination = $("#train-destination-input").val().trim();
-//     frequency = $("#frequency-train-input").val().trim();
-//     firstTrainTime = $("#first-train-time-input").val().trim();
-
-//     var newTrain = {
-//         trainName: trainName,
-//         destination: destination,
-//         frequency: frequency,
-//         firstTrainTime: firstTrainTime,
-//     };
-
-//     // Uploads employee data to the database
-//     database.ref().push(newTrain);
-//     // // Firebase watcher .on("child_added"
-
-//     console.log(newTrain.trainName);
-//     console.log(newTrain.destination);
-//     console.log(newTrain.firstTrainTime);
-//     console.log(newTrain.frequency);
-
-//     // Clears all of the text-boxes
-//     $("#train-name-input").val("");
-//     $("#train-destination-input").val("");
-//     $("#frequency-train-input").val("");
-//     $("#first-train-time-input").val("");
-// });
-
-// // database.ref().on("child_added", function (snapshot) {
-// //     console.log(childSnapshot.val());
-
-// database.ref().on("child_added", function(childSnapshot) {
-//     console.log(childSnapshot.val());
-
-//     var trainName = childSnapshot.val().trainName;
-//     var destination = childSnapshot.val().destination;
-
-
-//     var newRow = $("<tr>").append(
-//         $("<td>").text(sv.trainName),
-//         $("<td>").text(sv.destination),
-//         $("<td>").text(sv.frequency),
-//         $("<td>").text(sv.nextArrival),
-//         $("<td>").text(sv.minutesAway),
-//     )
-
-//     // Change the HTML to reflect
-//     $("#trains-table > tbody").append(newRow);
-
-
-//     // Handle the errors
-//     //  function (errorObject) {
-//     //     console.log("Errors handled: " + errorObject.code);
-//     //  }
-// });
-
 var config = {
     apiKey: "AIzaSyCAlUs1o7-3cpFCXQ_f_J7ymRlfe6N6myQ",
     authDomain: "train-tracker-03-12-19.firebaseapp.com",
@@ -91,7 +10,7 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
-// Initial Values
+// Initial variables
 var trainName = "";
 var destination = "";
 var firstTrainTime = 0;
@@ -99,17 +18,17 @@ var frequency = 0;
 var nextArrival = 0;
 var minutesAway = 0;
 
-// Capture Button Click
+// When the user clicks the submit button, this will happen
 $("#add-train-btn").on("click", function (event) {
     event.preventDefault();
 
-    // Grabbed values from text boxes
+    // We are going to grab the values from these ID's on the html
     trainName = $("#train-name-input").val().trim();
     destination = $("#train-destination-input").val().trim();
     frequency = $("#frequency-train-input").val().trim();
     firstTrainTime = $("#first-train-time-input").val().trim();
 
-    // Code for handling the push
+    // We are goin to push these variables to Firebase, they will be used again later
     database.ref().push({
         trainName: trainName,
         destination: destination,
@@ -117,7 +36,7 @@ $("#add-train-btn").on("click", function (event) {
         frequency: frequency,
     });
 
-    // Clears all of the text-boxes
+    // We clear the boxes where the user inputs the train information once they have submitted it, this betters the user experience
     $("#train-name-input").val("");
     $("#train-destination-input").val("");
     $("#frequency-train-input").val("");
@@ -125,23 +44,15 @@ $("#add-train-btn").on("click", function (event) {
 
 });
 
-// Firebase watcher .on("child_added"
+// When we add those variables to the database, the next lines of code happen
 database.ref().on("child_added", function (snapshot) {
-    // storing the snapshot.val() in a variable for convenience
+    // Storing the snapshot.val() in a variable for convenience
     var sv = snapshot.val();
-
-    // Console.loging the last user's data
-    console.log(sv.trainName);
-    console.log(sv.destination);
-    console.log(sv.firstTrainTime);
-    console.log(sv.frequency);
-
 
     // First Time (pushed back 1 year to make sure it comes before current time)
     var firstTrainTimeConverted = moment(sv.firstTrainTime, "HH:mm").subtract(1, "years");
-    console.log(firstTrainTimeConverted);
 
-    // Current Time
+    // Actual time
     var actualTime = moment();
     console.log("CURRENT TIME: " + moment(actualTime).format("HH:mm"));
 
@@ -149,17 +60,18 @@ database.ref().on("child_added", function (snapshot) {
     var timeDifference = moment().diff(moment(firstTrainTimeConverted), "minutes");
     console.log("DIFFERENCE IN TIME: " + timeDifference);
 
-    // Time apart (remainder)
+    // Time apart 
     var timeRemaining = timeDifference % sv.frequency;
     console.log(timeRemaining);
 
-    // Minute Until Train
+    // Minutes away (train)
     var minutesAway = sv.frequency - timeRemaining;
     console.log("MINUTES TILL TRAIN: " + minutesAway);
 
-    // Next Train
+    // When the next arrival is going to be
     var nextArrival =  moment().add(minutesAway, "minutes").format("HH:mm");
 
+    // Here we append these variables to the html page
     var newRow = $("<tr>").append(
         $("<td>").text(sv.trainName),
         $("<td>").text(sv.destination),
@@ -168,8 +80,7 @@ database.ref().on("child_added", function (snapshot) {
         $("<td>").text(minutesAway),
     )
 
-
-    // Change the HTML to reflect
+    // Here we update the table on the html to reflect the new trains added
     $("#trains-table > tbody").append(newRow);
 
 
@@ -178,4 +89,3 @@ database.ref().on("child_added", function (snapshot) {
     console.log("Errors handled: " + errorObject.code);
 });
 
-moment(nextArrival).format("HH:mm");
